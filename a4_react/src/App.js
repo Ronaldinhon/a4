b7a31ed95e47e5e33b06a2932dc1431b0e5bb0f8
Ronaldinhon <stanleychoo0522@hotmail.com>
2019-08-15 22:24:02 +0800

    Removed constants, actioncable and chat-window

--- a4_react/src/App.js
+++ a4_react/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ChatWindow from './ChatWindow';
 
 function App() {
   return (
@@ -19,7 +18,6 @@ function App() {
         >
           Learn React
         </a>
-        <ChatWindow/>
       </header>
     </div>
   );
--- a4_react/src/constants/index.js
+++
@@ -1,6 +0,0 @@
-export const API_ROOT = 'http://localhost:3000';
-export const API_WS_ROOT = process.env.API_WS_ROOT;
-export const HEADERS = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json',
-};
